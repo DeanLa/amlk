c8a606e64aa96e0281a35f257d47b65d0a23df0d
Daniel Levy <dlevy4585@gmail.com>
2017-10-25 09:34:53 +0300

    Fixed not following links issue with Crawler

--- domain_pages_fetcher/CrawlerSpider.py
+++ domain_pages_fetcher/CrawlerSpider.py
@@ -3,7 +3,6 @@ import scrapy
 from scrapy.spiders import CrawlSpider
 from random import shuffle
 
-
 class DomainSpider(CrawlSpider):
     name = "ynet"
     allowed_domains = ["www.ynet.co.il"]
@@ -15,19 +14,25 @@ class DomainSpider(CrawlSpider):
         links = LxmlLinkExtractor(allow=[".*" + self.name + ".*"]).extract_links(response)
         shuffle(links)
         for link in links:
-            if link not in self.visited:
-                self.visited.add(link)
+            if link.url not in self.visited:
+                self.visited.add(link.url)
                 yield scrapy.Request(link.url, callback=self.parse_dir_contents)
 
     def parse_dir_contents(self, response):
         if response.url not in self.parsed:
             self.parsed.add(response.url)
+        else:
+            return
 
         if self.allowed_domains[0] in response.url and "articles" in response.url:
             filename = "pages/" + response.url.split('/')[-1].replace(",", "_").replace("?", "_")
             with open(filename, 'wb') as f:
                 f.write(response.body)
 
-        # Continue to crawl links
-        self.parse(response)
+        links = LxmlLinkExtractor(allow=[".*" + self.name + ".*"]).extract_links(response)
+        shuffle(links)
+        for link in links:
+            if link.url not in self.visited:
+                self.visited.add(link.url)
+                yield scrapy.Request(link.url, callback=self.parse_dir_contents)
 
